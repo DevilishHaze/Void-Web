3f4cafb6855d640265463bb592e64b3383d65a72
Margaret <103428200+DevilishHaze@users.noreply.github.com>
2024-04-23 21:33:24 +0300

    'Импортировала Response,permissionDenied.Добавила функции в ArticleViewSet(viewsets.ModelViewSet).Изменила Условия в классе CommentViewSet.Также сделала кастомный класс доступа IsOwnerOrAdminPermission,который указала в permissions.py '

--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from .models import Articles, Comments, FavoriteArticle
 from .serializers import ArticleSerializer, CommentSerializer, FavoriteArticleSerializer
 
@@ -14,16 +16,30 @@ class ArticleViewSet(viewsets.ModelViewSet):
             permission_classes = [permissions.AllowAny]
         return [permission() for permission in permission_classes]
 
+    def retrieve(self , request , *args , **kwargs):  #получение одной статьи и комментов к ней
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        comments = Comments.objects.filter(article=instance)
+        comments_serializer = CommentSerializer(comments , many=True)
+        data = serializer.data
+        data['comments'] = comments_serializer.data
+        return Response(data)
+    def perform_create(self, serializer):
+        if self.request.user.is_superuser:
+            serializer.save(author=self.request.user)
+        else:
+            raise PermissionDenied("Only admins can create articles.\nТолько админы могут создавать статьи")
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comments.objects.all()
     serializer_class = CommentSerializer
 
     def get_permissions(self):
-        if self.action in ['create', 'update', 'partial_update']:
+        if self.action in ['create']:
             permission_classes = [permissions.IsAuthenticated]
-        elif self.action == 'destroy':
-            permission_classes = [permissions.IsAdminUser]
+        elif self.action  in ['update', 'partial_update', 'destroy']:
+            permission_classes = [permissions.IsOwnerOrAdminPermission]
         else:
             permission_classes = [permissions.AllowAny]
         return [permission() for permission in permission_classes]
@@ -32,14 +48,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 class FavoriteArticleViewSet(viewsets.ModelViewSet):
     queryset = FavoriteArticle.objects.all()
     serializer_class = FavoriteArticleSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_authenticated:
-            return FavoriteArticle.objects.filter(user=user)
-        else:
-            return FavoriteArticle.objects.none()
+    permission_classes = [permissions.IsOwnerOrAdminPermission]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
