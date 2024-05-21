ee6fa92fe45cc206311e9471ea81688974a47f73
Margaret <103428200+DevilishHaze@users.noreply.github.com>
2024-05-21 11:22:56 +0300

    'Избранные отображаются на странице пользователя в админ панели'

--- backend/main_app/admin.py
+++ backend/main_app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Comments, Articles
+
+from .models import Comments, Articles,FavoriteArticle
+
+class FavouriteArticleInline(admin.TabularInline):
+    model = FavoriteArticle
+    extra = 0
+admin.site.register(FavoriteArticle)
 admin.site.register(Comments)
 admin.site.register(Articles)
 
