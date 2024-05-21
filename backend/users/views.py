e50eb1a138626273fff5feb40473ad9efa2ca0fc
Margaret <103428200+DevilishHaze@users.noreply.github.com>
2024-05-21 11:27:18 +0300

    ..

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,11 +3,12 @@ from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
 
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAdminUser]
+
+
 
 
 
