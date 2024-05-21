41b5f5db2918e87080ddf9e6e198fda739257964
Margaret <103428200+DevilishHaze@users.noreply.github.com>
2024-05-21 11:25:50 +0300

    'изменение маршрутов '

--- backend/urls.py
+++ backend/urls.py
@@ -17,21 +17,31 @@ Including another URLconf
 from django.urls import path , include , re_path
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
-from backend.main_app.views import ArticleViewSet, CommentViewSet, FavoriteArticleViewSet
+from backend.main_app.views import CommentAPIView , ArticleListAPIView , \
+    CommentListApiView , ArticleUpdateAPIView , UserCommentListAPIView , FavoriteArticleAPIView , AddToFavoritesAPIView
 from backend.users.views import UserViewSet
 from .yasg import urlpatterns as doc_urls
-router = DefaultRouter()
+#router = DefaultRouter()
 
-router.register('api/articles', ArticleViewSet)
-router.register('api/comments', CommentViewSet)
-router.register('api/fav-articles', FavoriteArticleViewSet, basename='fav-article')
-router.register('api/users', UserViewSet, basename='user')
+#router.register('api/articles', ArticleViewSet)
+#router.register('api/comments', CommentViewSet)
+#router.register('api/fav-articles', FavoriteArticleViewSet, basename='fav-article')
+#router.register('api/users', UserViewSet, basename='user')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('articles/' , ArticleListAPIView.as_view() , name='article-list') ,
+    path('articles/<int:pk>/' , ArticleUpdateAPIView.as_view() , name='article-update') ,
+    path('comments/' , CommentListApiView.as_view() , name='comment-list') ,
+    path('comments/<int:pk>/' , CommentAPIView.as_view() , name='comment-detail') ,
+    path('articles/<int:article_id>/comments/', UserCommentListAPIView.as_view(), name='user-comment-list'),
+    path('favorites/' , FavoriteArticleAPIView.as_view() , name='favorite-article') ,
+    path('favorites/<int:pk>/' , FavoriteArticleAPIView.as_view() , name='favorite-article-detail') ,
+    path('articles/<int:pk>/add_to_favorites/', AddToFavoritesAPIView.as_view(), name='add-to-favorites'),
      ]
 
 urlpatterns += doc_urls
