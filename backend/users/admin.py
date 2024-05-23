1dc8a4289f4bc14b96d4ab853dee7b4f02cf90a6
Margaret <103428200+DevilishHaze@users.noreply.github.com>
2024-05-24 00:15:45 +0300

    Изменение отображения пароля

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from backend.main_app.admin import FavouriteArticleInline
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    inlines = [FavouriteArticleInline]
+
+    def change_view(self , request , object_id , form_url='' , extra_context=None):
+        self.exclude = ('password' ,)
+        return super().change_view(request , object_id , form_url , extra_context)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
