4fce9e15b5bfb66d6270c76e886e83c3111f7fc0
Margaret <103428200+DevilishHaze@users.noreply.github.com>
2024-06-13 20:33:02 +0300

    Настроены права доступа а также доп поле с юрл изображения и добавлена функция с получение юрл статьи созвездия

--- backend/constellations/serializers.py
+++ backend/constellations/serializers.py
@@ -1,12 +1,39 @@
 from rest_framework import permissions
 from rest_framework import serializers
-from .models import Constellation
 
-class ConstellationSerializer(serializers.ModelSerializer):
+from .models import Constellation
+from ..main_app.models import Articles
+
+class ConstellationForAdmin(serializers.ModelSerializer):
+    permission_classes = permissions.IsAdminUser
+    article_url = serializers.SerializerMethodField()
     class Meta:
         model = Constellation
-        fields = ['name', 'ra', 'dec']
+        fields = ['id','name','name_rus', 'ra', 'dec','article_url','image_url']
+
+    def get_article_url(self , obj):
+            request = self.context.get('request')
+            try:
+                article = Articles.objects.get(constellation=obj)
+                return request.build_absolute_uri(f'/articles/{article.id}/')
+            except Articles.DoesNotExist:
+                return None
+class ConstellationSerializer(serializers.ModelSerializer):
+    article_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Constellation
+        fields = ['name_rus', 'ra', 'dec','article_url', 'image_url']
+
+    def get_article_url(self, obj):
+        request = self.context.get('request')
+        try:
+            article = Articles.objects.get(constellation=obj)
+            return request.build_absolute_uri(f'/articles/{article.id}/')
+        except Articles.DoesNotExist:
+            return None
 
 class CoordinateSerializer(serializers.Serializer):
     longitude = serializers.FloatField(required=True)
     latitude = serializers.FloatField(required=True)
+
