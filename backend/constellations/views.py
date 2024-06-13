b98da4236cdf190fb721849a825e5a3a39841e55
Margaret <103428200+DevilishHaze@users.noreply.github.com>
2024-06-13 20:37:16 +0300

    добавлен комментарий

--- backend/constellations/views.py
+++ backend/constellations/views.py
@@ -2,14 +2,15 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status , permissions , generics
-from .serializers import CoordinateSerializer, ConstellationSerializer
+from .serializers import CoordinateSerializer , ConstellationSerializer , ConstellationForAdmin
 from .models import Constellation
 from .utils import get_visible_constellations
 
 
 class ConstellationsListAPI(generics.ListCreateAPIView):
+    "Только для админов"
     queryset = Constellation.objects.all()
-    serializer_class = ConstellationSerializer
+    serializer_class = ConstellationForAdmin
     permission_classes = [permissions.IsAdminUser]
 
 
@@ -31,8 +32,3 @@ class VisibleConstellationsAPI(APIView):
         else:
             print("Serializer errors:" , serializer.errors)  # Debugging line to print serializer errors
             return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
-            #constellation_serializer = ConstellationSerializer(visible_constellations, many=True,context={'request': request})
-            #return Response(constellation_serializer.data , status=status.HTTP_200_OK)
-        #else:
-           # print("Serializer errors:", serializer.errors)
-           # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
